2661096662ff39fea293e30923d192e42b896d81
marfiroozi <marfiroozi@gmail.com>
2022-09-03 15:05:10 +0430

    allowedhostchanged

--- doc_project/settings.py
+++ doc_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r5w)k$h=gi=r!co%qz+_%g33!36=!tg(%os7yccy-8o94!z1_x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
