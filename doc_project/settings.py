f9f9996400019a0eb62fa9c733bc8b503a428f48
marfiroozi <marfiroozi@gmail.com>
2022-09-03 19:36:07 +0430

    staticfiles

--- doc_project/settings.py
+++ doc_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from lib2to3.pytree import Base
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,7 +130,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ BASE_DIR / "static",'/var/www/static/',]
 
-STATIC_ROOT = str(BASE_DIR)+'/STATIC/'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_ROOT = str(BASE_DIR)+'/media/'
